3a2d7bc8ee735e9bfe0bd60daa24113a47967162
Brad Chao <brad@brad.tw>
2024-06-06 10:21:22 +0800

    06061021

--- brad99.php
+++ brad99.php
@@ -1,10 +1,12 @@
 <table border="1" width="100%">
     <tr>
         <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
+            <?php
+                for ($i = 1; $i <= 9; $i++){
+                    $r = 2 * $i;
+                    echo "2 x {$i} = {$r}<br />";
+                }
+            ?>
         </td>
         <td>
             2 x 1 = 2<br />
