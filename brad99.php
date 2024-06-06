d5434bf571812df4dbbf7af06063b9ae683ced42
Brad Chao <brad@brad.tw>
2024-06-06 10:13:36 +0800

    06061013

---
+++ brad99.php
@@ -0,0 +1,54 @@
+<table border="1" width="100%">
+    <tr>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+        <td>
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+            2 x 1 = 2<br />
+        </td>
+    </tr>
+</table>
