ec8863679f5b9975cc52950a4ed7830fb4ac44b7
Brad Chao <brad@brad.tw>
2024-06-06 11:09:31 +0800

    06061109

--- brad99.php
+++ brad99.php
@@ -1,9 +1,14 @@
 <table border="1" width="100%">
     <?php
-        for ($k = 0; $k < 2; $k++){
+        define('ROWS', 1);
+        define('COLS', 9);
+        define('START', 1);
+
+        //------------------------------------
+        for ($k = 0; $k < ROWS; $k++){
             echo '<tr>';
-            for($j = 2 ; $j <= 5 ; $j++ ){
-                $newj = $j + $k * 4;
+            for($j = START ; $j < START+COLS ; $j++ ){
+                $newj = $j + $k * COLS;
                 echo '<td>';
                 for ($i = 1; $i <= 9; $i++){
                     $r = $newj * $i;
