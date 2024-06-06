0892ea5738652ce9d2c9f75ce73d3b469f8c3ae2
Brad Chao <brad@brad.tw>
2024-06-06 11:21:49 +0800

    06061121

--- brad99.php
+++ brad99.php
@@ -1,18 +1,28 @@
+<?php
+    $rows = 2; $cols = 4; $start = 2;
+    if (isset($_GET['rows'])){
+        $rows = $_GET['rows'];
+        $cols = $_GET['cols'];
+        $start = $_GET['start'];
+    }
+
+?>
+
 <h1>Brad Big Company</h1>
 <hr />
 <form>
-    Rows: <input type="number" name="rows" />
-    Cols: <input type="number" name="cols" />
-    Start: <input type="number" name="start" />
+    Rows: <input type="number" name="rows" value="<?php echo $rows; ?>"/>
+    Cols: <input type="number" name="cols" value="<?php echo $cols; ?>"/>
+    Start: <input type="number" name="start" value="<?php echo $start; ?>"/>
     <input type="submit" value="Change" />
 </form>
 <hr />
 
 <table border="1" width="100%">
     <?php
-        define('ROWS', 1);
-        define('COLS', 9);
-        define('START', 1);
+        define('ROWS', $rows);
+        define('COLS', $cols);
+        define('START', $start);
 
         //------------------------------------
         for ($k = 0; $k < ROWS; $k++){
