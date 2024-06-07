98aad34b1ba44ec68dcf6339baf0c2fd11869525
Brad Chao <brad@brad.tw>
2024-06-07 14:33:45 +0800

    06071433

--- brad21.php
+++ brad21.php
@@ -1,19 +1,34 @@
 <?php
     $poker = range(0,51);
     shuffle($poker);
-    foreach($poker as $k => $v){
-        echo "{$k} : {$v}<br />";
-    }
 ?>
-<hr />
 <?php
     $players = [[],[],[],[]];  // array(array(),array(),array(),array())
     foreach($poker as $i => $card){
         $players[$i%4][(int)($i/4)] = $card;
     }
 
-    foreach($players[1] as $card){
-        echo "{$card}<br />";
+    for($i = 0; $i<count($players); $i++) {
+        sort($players[$i]);
     }
-
 ?>
+<table border="1" width="100%">
+    <?php
+        $colors = ['&spades;','<font color=red>&hearts;</font>',
+                '<font color=red>&diams;</font>','&clubs;'];
+        $values = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+        foreach($players as $player){
+            //sort($player);
+            echo '<tr>';
+            foreach($player  as $card){
+                $c = (int)($card / 13);
+                $v = $card % 13;
+                echo "<td>{$colors[$c]}{$values[$v]}</td>";
+            }
+            echo '</tr>';    
+        }
+    ?>
+</table>
+
+
+
