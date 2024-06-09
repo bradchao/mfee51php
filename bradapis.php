65eb311c5e585d13a0de60374516ba9a813af933
Brad Chao <brad@brad.tw>
2024-06-09 23:26:51 +0800

    ok

--- bradapis.php
+++ bradapis.php
@@ -1,4 +1,5 @@
 <?php
+define('LETTERS', 'ABCDEFGHJKLMNPQRSTUVXYWZIO');
 function checkTWId($id){
     $ret = false;
     // if (strlen($id) == 10){
@@ -13,8 +14,22 @@ function checkTWId($id){
     // }
 
     if (preg_match('/^[A-Z][12][0-9]{8}$/', $id)){
-        
-        $ret = true;
+        $c1 = substr($id, 0, 1);
+        $a12 = strpos(LETTERS, $c1) + 10;
+        $a1 = (int)($a12 / 10);
+        $a2 = $a12 % 10;
+        $n1 = substr($id, 1, 1);
+        $n2 = substr($id, 2, 1);
+        $n3 = substr($id, 3, 1);
+        $n4 = substr($id, 4, 1);
+        $n5 = substr($id, 5, 1);
+        $n6 = substr($id, 6, 1);
+        $n7 = substr($id, 7, 1);
+        $n8 = substr($id, 8, 1);
+        $n9 = substr($id, 9, 1);
+        $sum = $a1*1 + $a2*9 + $n1*8 + $n2*7 + $n3*6 + $n4*5 +
+            $n5*4 + $n6*3 + $n7*2 + $n8*1 + $n9*1;
+        $ret = $sum % 10 == 0;    
     }
 
     return $ret;
