0b8e87a25c212ea4b0ec6bad0a6ec5278528ad03
Brad Chao <brad@brad.tw>
2024-06-11 12:14:47 +0800

    06111214

---
+++ brad28.php
@@ -0,0 +1,19 @@
+<?php
+    // 1. 畫布
+    $img = imagecreatefromjpeg("imgs/coffee.jpg");
+
+    // 2. 作畫
+    $yellow = imagecolorallocate($img, 255,255,0);
+    imagettftext($img, 24, -30, 100, 300, $yellow, 'fonts/myfont.ttf', 
+        '資策會論壇專屬, 歡迎盜連');
+
+
+    // 3. 輸出
+    header("content-type: image/jpeg");
+    imagejpeg($img);
+    
+    imagejpeg($img, 'dir1/mycoffee.jpg');
+
+    // 4. 清除
+    imagedestroy($img);
+?>
