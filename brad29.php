273b527c1811cf27527332af566fd85173980d4c
Brad Chao <brad@brad.tw>
2024-06-11 15:37:29 +0800

    06111537

--- brad29.php
+++ brad29.php
@@ -1,10 +1,33 @@
 <?php
+    date_default_timezone_set('Asia/Taipei');
+?>
+<table border="1" width="100%">
+    <tr>
+        <th>Filename</th>
+        <th>Type</th>
+        <th>Time</th>
+    </tr>
+<?php
     $fp = @opendir("c:/MAMP/htdocs") or die('Server Busy');
     //var_dump($fp);
 
     while ($fname = readdir($fp)){
-        echo "{$fname}<br />";
+        echo '<tr>';
+        echo "<td>{$fname}</td>";
+        if (is_dir($fname)){
+            echo "<td>Dir</td>";
+        }else if (is_file($fname)){
+            echo "<td>File</td>";
+        }else{
+            echo "<td></td>";
+        }
+
+        $mtime = date('Y-m-d H:i:s', filemtime($fname));
+        echo "<td>{$mtime}</td>";
+
+        echo '</tr>';
     }
 
     closedir($fp);
 ?>
+</table>
