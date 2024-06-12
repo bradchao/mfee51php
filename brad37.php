6a67ba5ea2e0137683b070df5d03514c71e0ebbd
Brad Chao <brad@brad.tw>
2024-06-12 16:31:15 +0800

    06121600

--- brad37.php
+++ brad37.php
@@ -1,5 +1,19 @@
 <?php
     $json = file_get_contents('https://data.moa.gov.tw/Service/OpenData/ODwsv/ODwsvAgriculturalProduce.aspx');
     //echo $json;
+    $data = json_decode($json);
+
+    $mysqli = new mysqli('localhost','root','root', 'iii');
+    $mysqli->set_charset('utf8');   
+    
+    $mysqli->query('DELETE FROM gift');
+    $mysqli->query('ALTER TABLE gift AUTO_INCREMENT = 1');
+
+    //var_dump($data);
+    foreach($data as $gift){
+        $sql = "INSERT INTO gift (name,addr,picurl)" . 
+            " VALUES ('{$gift->Name}','{$gift->SalePlace}','{$gift->Column1}')";
+        $mysqli->query($sql);
+    }
 
 ?>
